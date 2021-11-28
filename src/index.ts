9f4252e175e25d7316ad91150bc7e750cda0391c
Cerber Ursi <dev+node@cerbe.ru>
2021-11-28 13:06:49 +0700

    Three-step store building

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ type Enum<T extends object> = {
         : { readonly discriminant: K; readonly data: T[K] };
 }[keyof T];
 
+type Acts<Values> = Record<string, (this: Values, ...args: any[]) => any>;
+
 function objectAssign(to: object, from: object | null) {
     if (from !== null) {
         for (let key of Object.getOwnPropertyNames(from)) {
@@ -17,11 +19,16 @@ function objectAssign(to: object, from: object | null) {
     }
 }
 
-export function makeStoreFactory<Values extends {}, Actions extends {}>(def: Values, actions: Actions) {
-    return function(values: Partial<Values>): Values & Actions {
-        const obj = Object.create(actions);
-        objectAssign(obj, def);
-        objectAssign(obj, values);
-        return obj;
+export function prepareStoreFactory<Values extends {}>(defaults: Values) {
+    return function hydrate<Actions extends Acts<Values>>(actions: Actions) {
+        return function make(values: Partial<Values>): Values & Actions {
+            // Workaround for https://github.com/mobxjs/mobx/issues/3197
+            actions.constructor = () => {};
+
+            const obj = Object.create(actions);
+            objectAssign(obj, defaults);
+            objectAssign(obj, values);
+            return obj;
+        }
     }
 }
--- tests/smoke.test.ts
+++ tests/smoke.test.ts
@@ -1,29 +1,18 @@
 import { makeAutoObservable, reaction, runInAction } from "mobx";
-import { makeStoreFactory } from "..";
-
-interface StoreValues {
-    value: number;
-    readonly double: number;
-}
-interface StoreActions {
-    inc(this: StoreValues): void;
-}
+import { prepareStoreFactory } from "..";
 
 describe("Store builder", () => {
     it("Is built correctly for simple types", () => {
-        const store = makeStoreFactory<StoreValues, StoreActions>(
-            {
-                value: 0,
-                get double() {
-                    return 2 * this.value;
-                },
+        const store = prepareStoreFactory({
+            value: 0,
+            get double() {
+                return 2 * this.value;
             },
-            {
-                inc() {
-                    this.value += 1;
-                },
-            }
-        )({});
+        })({
+            inc() {
+                this.value += 1;
+            },
+        })({});
 
         store.inc();
         expect(store.value).toBe(1);
@@ -36,6 +25,8 @@ describe("Store builder", () => {
         const react = jest.fn();
         reaction(() => store.double, react);
         store.inc();
-        expect(react.mock.calls.map(call => [call[0], call[1]])).toEqual([[6, 4]]);
+        expect(react.mock.calls.map((call) => [call[0], call[1]])).toEqual([
+            [6, 4],
+        ]);
     });
 });
